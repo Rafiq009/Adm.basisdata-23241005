ab8f5c4164e451f18af028fb87cd433ff872af05
Rafiq009 <145946261+Rafiq009@users.noreply.github.com>
2025-04-28 10:21:49 +0800

    Add files via upload

---
+++ pre-praktikum/pre2.sql
@@ -0,0 +1,26 @@
+-- nama : irsyad hakki
+-- nim :23241004
+-- kelas: A
+
+-- Untuk melihat databases di mysql
+SHOW DATABASES;
+
+-- melihat databases yang digunakan
+
+USE mysql;
+
+-- menampilkan table dalam databases
+
+SHOW TABLES;
+
+-- MENDISKRIPSIKAN TABLE
+
+DESCRIBE user;
+
+-- menampilkan isi data dari kolom user 
+
+SELECT Host ,User ,Password FROM user;
+
+ 
+   
+
